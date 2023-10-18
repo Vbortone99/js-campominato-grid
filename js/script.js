bfe442beeab9eccbdd7bdccd25c099115b7d09ed
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-18 23:00:24 +0200

    html structure + css settings

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// 2. Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// 3. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+const btn = document.querySelector('button');
