71809de480f019deebf422845f7d609a84ee7258
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-18 23:10:50 +0200

    added function to draw the squares + square active

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@
 // 3. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 const btn = document.querySelector('button');
+
+// Evento Bottone
+btn.addEventListener('click',function(){
+    // numero di quadrati
+    const squareNumber = 64;
+    // Griglia
+    const playground = document.getElementById('playground');
+    // Stampa dei Quadrati
+    for(let i = 0; i < squareNumber; i++){
+        let square = drawSquare(i);
+        console.log(square);
+    }
+})
+
+// FUNCTION STAMPA DEI QUADRATI
+function drawSquare(squareI){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = squareI + 1;
+    // ACTIVE SUL CLICK AL QUADRATOO
+    square.addEventListener('click',function(){
+        square.classList.add('active');
+    });
+    return square;
+};
